5ed7944afc06538709fd602bd4b7762a751488ab
Paolo Veronelli <paolo.veronelli@gmail.com>
2011-04-07 22:13:09 +0200

    fixed cabal to compile Test. Manager not uptodate

--- Dependency/Manager.hs
+++ Dependency/Manager.hs
@@ -3,6 +3,7 @@
 module Dependency.Manager (Item (Item), Manager, insertItems, deleteItems, touchItems, update, newManager)  where
 
 import Control.Applicative ((<$>))
+import Control.Monad (foldM)
 import Dependency.Core
 
 -- | Abstract client core monadic data, must be supplied. Clients can choose monad and index type.
@@ -13,6 +14,15 @@ data Ord b => Item m b = Item
   , depmask :: b -> Bool  -- ^ dependencies selector
   }
 
+-- | Manager object. First 3 methods are pure and prepare the monadic update, which is the last method.
+data Manager m b = Manager {
+  insertItems :: [Item m b] -> Either IndexError (Manager m b),
+  deleteItems :: [b] -> Manager m b,
+  touchItems :: [b] -> Manager m b,
+  update :: m (Either IndexError (Manager m b))
+  }
+  
+{- Not uptodate
 -- Compile all (hiding some results)
 compile :: (Ord b, Functor m, Monad m) => Core b (Item m b) -> m (Core b (Item m b))
 compile d = case step d of
@@ -22,22 +32,17 @@ compile d = case step d of
   (Build x,d') -> foldr f d' <$> build x where
     f (c@(Item k _ _ dm)) d = create d k c dm (Just $ index x)
 
--- | Manager object. First 3 methods are pure and prepare the monadic update, which is the last method.
-data Manager m b = Manager {
-  insertItems :: [Item m b] -> Manager m b,
-  deleteItems :: [b] -> Manager m b,
-  touchItems :: [b] -> Manager m b,
-  update :: m (Manager m b)
-  }
-
+-}
 -- | Create a fresh management, with no items controlled
 newManager ::  (Ord b , Functor m, Monad m) => Manager m b
+newManager = undefined
+{-
 newManager = let
   insertItems' x = mkManager . news x
-  news d xs = foldr f d xs where   f (c@(Item k _ _ dm)) d = create d k c dm Nothing
+  news d xs = foldM f d xs where   f (c@(Item k _ _ dm)) d = create d k c dm Nothing
   deleteItems' x = mkManager . foldr (flip delete) x
   touchItems' x = mkManager . foldr (flip touch) x
   update' x = mkManager <$> compile x
   mkManager x = Manager (insertItems' x) (deleteItems' x) (touchItems' x) (update' x)
   in mkManager mkCore
-
+-}
